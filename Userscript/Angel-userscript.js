bbc60c1a224fbf32d5d5a84c80275d2cc056fac0
bacon-hub <ly5878259@gmail.com>
2026-04-10 09:36:36 +0700

    Update Angel-userscript.js

--- Userscript/Angel-userscript.js
+++ Userscript/Angel-userscript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Angel Bypass
-// @namespace    Discord https://discord.gg/Td27T6uR9b | bot https://angel-invite.vercel.app/
-// @version      1.0.0
-// @description  Angel Bypass free version
+// @namespace    Angel.l0l bypass
+// @version      1.1.7
+// @description  Angel free version
 // @author       Angel.l0l
-// @icon         https://cdn.discordapp.com/attachments/1475714943033802906/1476164609319768094/5C7558E7-2E1A-4F6B-9F19-464672F9D6D7.png?ex=69a60fac&is=69a4be2c&hm=631d5fb8a00ea2485a6ebf9fa38c8b4ab3a47098a8b823b93c7a4ed6b0701050&
+// @icon         https://cdn.discordapp.com/attachments/1472797429278703779/1489609882507153439/5C7558E7-2E1A-4F6B-9F19-464672F9D6D7.png?ex=69d10acf&is=69cfb94f&hm=3f3e588e5492cc1d6f035ea6ca3787ee3a3466baa8e0d904a5a9326ebd3aa344&
 // @match        *://*.rekonise.com/*
 // @match        *://*.linkvertise.com/*
 // @match        *://*.paster.so/*
@@ -31,10 +31,9 @@
 // @match        *://*.tinylink.onl/*
 // @match        *://*.tinyurl.com/*
 // @match        *://*.v.gd/*
-// @match        *://*.work.ink/*
 // @updateURL    https://raw.githubusercontent.com/ly5878259-cpu/Angel-userscript/main/Userscript/Angel-userscript.js
 // @downloadURL  https://raw.githubusercontent.com/ly5878259-cpu/Angel-userscript/main/Userscript/Angel-userscript.js
 // @grant        GM_xmlhttpRequest
 // @connect      ace-bypass.com
-// @// @require https://raw.githubusercontent.com/ly5878259-cpu/angel-main/main/Api/main.js
+// @require https://raw.githubusercontent.com/ly5878259-cpu/angel-main/main/Api/main.js
 // ==/UserScript==
